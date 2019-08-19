640ecd810f272010bdaa568a4cad97f5be5ed64b
lyohan <jik0090@naver.com>
2019-08-19 17:30:31 +0900

    complete lecture 2-8

--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -3,3 +3,23 @@
 *    Mastering Data Visualization with D3.js
 *    2.8 - Activity: Your first visualization!
 */
+
+d3.json('data/buildings.json').then(data => {
+  data.forEach(d => d.height = +d.height )
+
+  const svg = d3.select("#chart-area")
+                .append("svg")
+                .attr("width", "500px")
+                .attr("height", "500px")
+
+  svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * 30 )
+      .attr('y', 50)
+      .attr('width', 20)
+      .attr('height', d => d.height)
+      .attr('fill', 'grey')
+}) 
+
